3c547a4a1bdf282d88032a42de6ab90c5e7e4e03
exalab999 <exalabdevelopers@gmail.com>
2020-08-27 15:08:43 +0800

    Bug fixed

--- Scripts/Installer/Arch/armhf/arch.sh
+++ Scripts/Installer/Arch/armhf/arch.sh
@@ -79,6 +79,6 @@ echo "removing image for some space"
 rm $tarball
 echo "You can now launch Arch Linux with the ./${bin} script"
 echo "Preparing additional component for the first time, please wait..."
-wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/amd64/resolv.conf" -P arch-fs/root
-wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/amd64/additional.sh" -P arch-fs/root
+wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/resolv.conf" -P arch-fs/root
+wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/additional.sh" -P arch-fs/root
 echo "done"
