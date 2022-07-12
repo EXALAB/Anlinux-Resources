17f35ffc1b23f07cae8bfffca87651f6fb820d69
exalab999 <exalabdevelopers@gmail.com>
2022-07-12 15:42:00 +0800

    Testing Gnome

--- Experimental/README.md
+++
@@ -1,3 +0,0 @@
-# WARNING
-
-Everything under this folder is extremely experimental, no one knows what error it could cause unless after testing, please don't use anything under this folder unless you are a developer.
--- Experimental/de-ubuntu-gnome.sh
+++ Experimental/de-ubuntu-gnome.sh
@@ -2,8 +2,17 @@
 
 #Get the necessary components
 apt-get update
-apt-get install kde-plasma-desktop tigervnc-standalone-server dbus-x11 -y
-apt-get install xfe -y
+apt-get install xorg ubuntu-gnome-desktop tigervnc-standalone-server dbus-x11 -y
+
+echo "Proccesing command provided by @maalos, this fixed error in Ubuntu 20 Focal."
+
+rm /var/lib/dpkg/info/fprintd.postinst
+rm /var/lib/dpkg/info/libfprint*.postinst
+rm /var/lib/dpkg/info/libpam-fprintd*.postinst
+dpkg --configure -a
+
+echo "Done."
+
 apt-get clean
 
 #Setup the necessary files
@@ -25,7 +34,7 @@ echo "The VNC Server will be started at 127.0.0.1:5901"
 echo " "
 echo "You can connect to this address with a VNC Viewer you prefer"
 echo " "
-echo "Connect to this address will open a window with KDE Desktop Environment"
+echo "Connect to this address will open a window with Gnome Desktop Environment"
 echo " "
 echo " "
 echo " "
