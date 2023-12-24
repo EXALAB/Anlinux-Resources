11f1d62e90b75a95f8d770c37c0eefdd33fb6cee
exalab999 <exalabdevelopers@gmail.com>
2023-12-24 10:11:28 +0800

    Bug Fixed

--- Scripts/Installer/CentOS/centos.sh
+++ Scripts/Installer/CentOS/centos.sh
@@ -24,7 +24,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/CentOS/${archurl}/centos_stream-rootfs-${archurl}.tar.xz" -O $tarball
+		wget "https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/CentOS/${archurl}/centos-rootfs-${archurl}.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
