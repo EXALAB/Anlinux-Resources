75b11da8a4f26a1cfb04f0eb2d6847a1be82eab2
1Q23LYC45 <143373643+1Q23LYC45@users.noreply.github.com>
2024-02-02 19:35:36 +0800

    Fix: Firefox ESR Crash on Debian with user "root"

--- Scripts/Installer/Debian/debian.sh
+++ Scripts/Installer/Debian/debian.sh
@@ -65,6 +65,7 @@ command+=" HOME=/root"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=\$TERM"
 command+=" LANG=C.UTF-8"
+command+=" MOZ_FAKE_NO_SANDBOX=1"
 command+=" /bin/bash --login"
 com="\$@"
 if [ -z "\$1" ];then
